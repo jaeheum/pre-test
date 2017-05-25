273dffb7466fca37818db214c8e1a0c0ea055a06
Jay Han <jay.han@gmail.com>
2017-05-25 09:10:16 -0700

    rm unnecessary files

--- t/h/doublefree.h
+++
@@ -1,2 +0,0 @@
-#include<stdlib.h>
-void doublefree(size_t);
--- t/h/doublefree1.c
+++
@@ -1,9 +0,0 @@
-#include"doublefree.h"
-void doublefree(size_t n){
-  unsigned char*x=(unsigned char*)malloc(n);
-  free(x);
-  if(!x){
-    free(x);
-  }
-  return;
-}
